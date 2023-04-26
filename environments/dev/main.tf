c60a96b0b69f2a9def6e67ee2bebcd60a0e0bb06
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-04-25 20:30:23 -0500

    the main.tf was modified

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -10,6 +10,9 @@ provider "google" {
 module "network" {
   source = "../../modules/network"
 
+  project = var.project_id
+  region  = var.region
+  network_name = var.network_name
   subnet_name            = var.subnet_name
   subnet_cidr            = var.subnet_cidr
   firewall_rule_name     = var.firewall_rule_name
