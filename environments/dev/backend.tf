bf1a16a8ee00bd6732b87453397ccafbe48902fc
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-04-25 19:49:05 -0500

    the backend.tf was modified

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.25.0"
+    }
+  }
+  
   backend "gcs" {
     bucket = "pepito-dev-tfstate"
     prefix = "env/dev"
