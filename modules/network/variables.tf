1bc6a9a4b35b409a7f810e9efe72701b464e619f
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-04-26 11:06:35 -0500

    the variables.tf file of the network module with the variables was edited

---
+++ modules/network/variables.tf
@@ -0,0 +1,49 @@
+variable "project_id" {
+  type          = string
+  description   = "The ID of the project"
+}
+
+variable "region" {
+  type          = string 
+  description   = "The region of the project"
+}
+
+variable "network_name" {
+  type          = string
+  description   = "The network name"
+}
+
+variable "subnet_name" {
+  type          = string
+  description   = "The subnet name"
+}
+
+variable "subnet_cidr" {
+  type          = string
+  description   = "The subnet IP address range"
+}
+
+variable "firewall_rule_name" {
+  type          = string
+  description   = "The firewall rule name"
+}
+
+variable "firewall_rule_protocol" {
+  type          = string
+  description   = "Protocol allowed in firewall rule"
+}
+
+variable "firewall_rule_ports" {
+  type          = list(string)
+  description   = "Ports allowed in the firewall rule"
+}
+
+variable "env" {
+  type = string
+  description   = "the environment"
+}
+
+variable "address_name" {
+  type = string
+  description = "The IP address name"
+}
