2543be1a4587482a2a443129504babd59f56d6b3
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-04-25 21:12:40 -0500

    the main.tf was modified

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -8,14 +8,9 @@ provider "google" {
 }
 
 module "network" {
-  source = "../modules/network"
+  source = "../../modules/network"
 
-  project_id = var.project_id
-  region  = var.region
-  network_name = var.network_name
-  subnet_name            = var.subnet_name
-  subnet_cidr            = var.subnet_cidr
-  firewall_rule_name     = var.firewall_rule_name
-  firewall_rule_protocol = var.firewall_rule_protocol
-  firewall_rule_ports    = var.firewall_rule_ports
+  project_id = "pepito-dev"
+  region = "us-central1"
+  network_name = "${local.env}-vpc"
 }
