7687360b330f66fb64b9c2dda2da95f81209e485
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-04-25 21:36:41 -0500

    the main.tf was modified in the line 1

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,4 +1,4 @@
-resource "google_project_service" " compute" {
+resource "google_project_service" "compute" {
   project = var.project_id
   service = "compute.googleapis.com"
   disable_on_destroy = true
