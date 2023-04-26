02807bdc43bfac175af3f8e8576f8ffc319a0831
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-04-25 21:21:26 -0500

    the main.tf was modified

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -13,4 +13,9 @@ module "network" {
   project_id = "pepito-dev"
   region = "us-central1"
   network_name = "${local.env}-vpc"
+  subnet_name = "${local.env}-subnet"
+  subnet_cidr = "192.168.0.0/16"
+  firewall_rule_name = "${local.env}-firewall"
+  firewall_rule_protocol = "tcp"
+  firewall_rule_ports = ["80", "443"]
 }
