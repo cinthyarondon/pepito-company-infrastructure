5f2de7f43795351351f926adf4fb16da5d33ebca
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-04-26 19:20:00 -0500

    the google module was modified

--- modules/google/variables.tf
+++ modules/google/variables.tf
@@ -7,3 +7,13 @@ variable "region" {
   type = string
   description = "The GCP region to use"
 }
+
+variable "bucket" {
+  type = string
+  description = "The name of the GCS bucket to use as the backend"
+}
+
+variable "prefix" {
+  type = string
+  description = "The prefix to use for storing the Terraform state file in the GCS bucket"
+}
