0290e447eb27cdaaec9309612d2e79927170a901
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-04-26 19:40:03 -0500

    some necessary values were added in the main.tf of the network module

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -13,9 +13,9 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_compute_subnetwork" "subnet" {
+  project       = var.project_id
   name          = var.subnet_name
   ip_cidr_range = var.subnet_cidr
-  project       = var.project_id
   region        = var.region
   network       = google_compute_network.vpc_network.self_link
 }
