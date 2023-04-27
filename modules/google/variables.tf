a017179f324e3c7315dc4ae5a37ad75cb74ce7c6
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-04-26 19:31:55 -0500

    the bucket variable and prefix variable were deleted

--- modules/google/variables.tf
+++ modules/google/variables.tf
@@ -7,13 +7,3 @@ variable "region" {
   type = string
   description = "The GCP region to use"
 }
-
-variable "bucket" {
-  type = string
-  description = "The name of the GCS bucket to use as the backend"
-}
-
-variable "prefix" {
-  type = string
-  description = "The prefix to use for storing the Terraform state file in the GCS bucket"
-}
