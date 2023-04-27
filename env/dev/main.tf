b39f45faa605846c2524d230ae7d0f0b3c4ac9b0
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-04-27 13:20:58 -0500

    the main.tf file of the dev environment was edited in the line 63

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -65,8 +65,8 @@ module "gke" {
   subnet_name            = module.network.subnet_name
   subnet_cidr            = module.network.subnet_cidr
   firewall_rule_name     = module.network.firewall_rule_name
-  firewall_rule_protocol = module.network.firewall_rule_protocol
-  firewall_rule_ports    = module.network.firewall_rule_ports
+  firewall_rule_protocol = [module.network.firewall_rule_protocol]
+  firewall_rule_ports    = [module.network.firewall_rule_ports]
 }
 
 output "firewall_rule_protocol" {
