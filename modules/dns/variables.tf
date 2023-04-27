34b9bd17555c218b149b9755b1629b66f5a4f52c
Cinthya Rondon <cinthyamayerly@gmail.com>
2023-04-26 23:01:59 -0500

    the dns module was created

---
+++ modules/dns/variables.tf
@@ -0,0 +1,36 @@
+#variable "app_names" {
+#  type    = list(string)
+#  #default = ["app1", "app2", "app3", "monitor"]
+#}
+
+variable "environment_name" {
+  type    = string
+  #default = ["dev", "stage", "prod"]
+}
+
+variable "zone_name" {
+  type        = string
+  description = "The DNS zone name"
+}
+
+variable "domain" {
+  type        = string
+  description = "The domain name"
+}
+
+#variable "record_type" {
+#  type        = string
+#  description = "DNS record type"
+#  default     = "A"
+#}
+
+#variable "ttl" {
+#  type        = number
+#  description = "TTL (time to live) of the DNS record"
+#  default     = 300
+#}
+
+variable "address_name" {
+  type = string
+}
+
